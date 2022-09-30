6ffbb17fbb524494cf07dbb898e5408a2d4a56a5
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2022-09-30 22:49:25 +0530

    day-3

--- src/Components/useMemo.jsx
+++ src/Components/useMemo.jsx
@@ -1,17 +1,29 @@
 import React,{useMemo,useState} from 'react'
 
-const Fact = (n) =>{
-    let ans = 1;
-    for(var i = n; i>=1;i--){
-        ans= ans*i;
-    }
-    console.log('Factorial');
-    return ans;
-}
+// const Fact = (n) =>{
+//     let ans = 1;
+//     for(var i = n; i>=1;i--){
+//         ans= ans*i;
+//     }
+//     console.log('Factorial');
+//     return ans;
+// }
+function Fact (num) { 
+    let result = 1;
+     while(num > 0) 
+     { 
+        result = result * num;
+         num = num - 1; 
+        } 
+        // console.log(result);
+        return result;
+     }
 function Memo() {
     const [count,setCount] = useState(0);
-    const [num,setNum] = useState(5);
-    const factorial = useMemo(() =>{Fact(num)},[num])
+    // const [num,setNum] = useState(5);
+    const factorial = useMemo(() => Fact(count),[count])
+    // const factorial = Fact(num);
+    console.log(factorial)
   return (
     <div>
         <h2>What is UseMemo Hook ?(Implementation)</h2>
@@ -21,10 +33,11 @@ function Memo() {
             <li>This can improve performance.</li>
         </ul>
         <h3>Example:-</h3>
-        <h1>count :{count}</h1>
+        <h1>count :{count }</h1>
+        {/* {console.log(factorial)} */}
         <h1>count Factorial : {factorial} <br/>
-        <button type="button" className="btn btn-danger" onClick={() => setCount((c) => c + 1)}>Count +</button>
-        <button type="button" className="btn btn-primary" onClick={() => setNum((c) => c + 1)}>Num +</button>
+        <button type="button" className="btn btn-primary" onClick={() => setCount((c) => c + 1)}>Count +</button>
+        {/* <button type="button" className="btn btn-primary" onClick={() => setNum((c) => c + 1)}>Num +</button> */}
         </h1>
        </div>
   )
